21dd91b7bf2cebd4f872d12e902e7b84bc460c1f
tomekk001 <tomekczyz001@wp.pl>
2024-10-18 13:16:33 +0200

    łączenie z bazą

--- Projekt/src/server.js
+++ Projekt/src/server.js
@@ -1,29 +1,27 @@
-// //server.js
-// const express = require('express');
-// const cors = require('cors');
-// const mysql = require('mysql');
-// const app = express();
-// const PORT = 3000;
+const MongoClient = require("mongodb").MongoClient;
+const express = require("express");
+const { ftruncate } = require("fs");
+const path = require("path");
 
+const app = express();
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "/../projekt")));
 
-// // Middleware
-// app.use(cors());
-// app.use(express.json()); // Do obsługi danych w formacie JSON
+const uri = "mongodb://localhost:27017/";
+const dbName = "userAuthDB";
+const collections = "users";
+const port = 3000;
 
-// // Połączenie z bazą MySQL
-// const db = mysql.createConnection({
-//     host: 'MaciekMojsa',    // lub adres serwera MySQL
-//     user: 'Projekt1',         // użytkownik bazy danych MySQL
-//     password: 'admin', // hasło do bazy danych
-//     database: 'projekt',// nazwa bazy danych
-    
-// });
+async function ConnectToDb() {
+  const client = new MongoClient(uri);
+  try {
+    await client.connect();
+    console.log("Połączenie otwarte");
+    const db = client.db(dbName);
+    return db;
+  } catch (err) {
+    console.error(err);
+  }
+}
 
-// db.connect((err) => {
-//     if (err) {
-//         console.error('Błąd połączenia z MySQL: ', err);
-//         return;
-//     }
-//     console.log('Połączono z bazą MySQL');
-// });
-
+app.listen(port, () => console.log(`Serwer działa na porcie ${port}`));
